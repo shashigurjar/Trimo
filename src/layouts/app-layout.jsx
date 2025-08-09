b6ae93391bad9b310b703b957b34344d2459eb7f
Shashank Gurjar <sg243gurjar@gmail.com>
2025-08-10 01:41:14 +0530

    Initial commit

---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,15 @@
+import Header from "@/components/header";
+import {Outlet} from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <main className="min-h-screen container">
+        <Header />
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default AppLayout;
