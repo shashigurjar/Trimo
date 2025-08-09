8e14ba219ac4f06633a1048b56667fa7af60bd79
Shashank Gurjar <123729772+shashigurjar@users.noreply.github.com>
2025-08-10 01:53:00 +0530

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
     <>
       <nav className="py-4 flex justify-between items-center">
         <Link to="/">
-          <img src="/logo.png" className="h-16" alt="Trimrr Logo" />
+          <img src="/fox.png" className="h-16" alt="Trimrr Logo" />
         </Link>
         <div className="flex gap-4">
           {!user ? (
